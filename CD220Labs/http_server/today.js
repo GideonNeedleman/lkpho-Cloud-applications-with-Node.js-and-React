81bca7904d47abc9601944a22560fba620a4b96e
Gideon Needleman <111554925+GideonNeedleman@users.noreply.github.com>
2023-04-26 14:43:29 -0700

    finished lab

--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -1,4 +1,4 @@
 module.exports.getDate = function getDate() {
-    let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
+    let aestTime = new Date().toLocaleString("en-US", {timeZone: "PST"}); // not sure how to do pacific daylight time
     return new Date(aestTime);
 }
